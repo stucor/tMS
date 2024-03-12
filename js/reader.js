f24483226f7701f7995e867b6cd96ba5a9bfea8f
stucor <43881178+stucor@users.noreply.github.com>
2024-03-12 16:32:56 +0000

    sections functionality

--- js/reader.js
+++ js/reader.js
@@ -342,6 +342,12 @@ function buildInfo () {
 		let shortCode = shortcode();
 		let html ='';
 
+/* 		let sections = document.getElementsByClassName('goselfquote');
+		
+		for (let i = 0; i <sections.length; i++) {
+			html += sections[i].innerText.replace('§', '') + "<br>";
+		  }
+ */
 		function suttalist () {
 			let suttarefArr = document.getElementsByClassName('sclinktext');
 			if (suttarefArr.length == 0) { return '';}
@@ -3043,19 +3049,428 @@ function displaySelfquote (linktext) {
 	modalbody.scrollTop = 0;
 	stopBookScroll ();
 	let selfquoteArea = document.getElementById("selfquotearea");
+	
+
 	let buildHTML = '';
+
+	//console.log(linktext.slice(1))
+
+
+
 	if (linktext.substring(0,1) == '§') {
+		let SQHeaderArray = [
+			{
+				"section": "2",
+				"modalHead": "Section",
+				"bodyHead": "Multiple Suttas",
+				"supHTML": ""
+			},
+			{
+				"section": "5",
+				"modalHead": "Mahāhatthipadopamasutta",
+				"bodyHead": "The Longer Simile of the Elephant’s Footprint",
+				"supHTML": ""
+			},
+			{
+				"section": "6",
+				"modalHead": "Samādhisutta",
+				"bodyHead": "Development of Immersion",
+				"supHTML": ""
+			},
+			{
+				"section": "7",
+				"modalHead": "Uppādasutta",
+				"bodyHead": "Arising",
+				"supHTML": ""
+			},
+			{
+				"section": "8",
+				"modalHead": "Paṭhamadabbasutta",
+				"bodyHead": "With Dabba (1st)",
+				"supHTML": ""
+			},
+			{
+				"section": "11",
+				"modalHead": "Gotamasutta",
+				"bodyHead": "Gotama",
+				"supHTML": ""
+			},
+			{
+				"section": "17",
+				"modalHead": "Kappattheragāthā",
+				"bodyHead": "Kappa",
+				"supHTML": ""
+			},
+			{
+				"section": "20",
+				"modalHead": "Vibhaṅgasutta",
+				"bodyHead": "Analysis",
+				"supHTML": ""
+			},
+			{
+				"section": "23",
+				"modalHead": "Atthirāgasutta",
+				"bodyHead": "If There is Desire",
+				"supHTML": ""
+			},
+			{
+				"section": "24",
+				"modalHead": "Mahāvedallasutta",
+				"bodyHead": "The Great Classification",
+				"supHTML": ""
+			},
+			{
+				"section": "25",
+				"modalHead": "Titthāyatanasutta",
+				"bodyHead": "Sectarian Tenets",
+				"supHTML": ""
+			},
+			{
+				"section": "26",
+				"modalHead": "Mahātaṇhāsaṅkhayasutta",
+				"bodyHead": "The Longer Discourse on the Ending of Craving",
+				"supHTML": ""
+			},
+			{
+				"section": "33",
+				"modalHead": "Dutiyagaddulabaddhasuttam",
+				"bodyHead": "A Leash (2nd)",
+				"supHTML": ""
+			},
+			{
+				"section": "35",
+				"modalHead": "Upādānaparipavattasutta",
+				"bodyHead": "Perspectives",
+				"supHTML": ""
+			},
+			{
+				"section": "49",
+				"modalHead": "Natumhasutta",
+				"bodyHead": "Not Yours",
+				"supHTML": ""
+			},
+			{
+				"section": "53",
+				"modalHead": "Cetanāsutta",
+				"bodyHead": "Intention",
+				"supHTML": ""
+			},
+			{
+				"section": "54",
+				"modalHead": "Dutiyacetanāsutta",
+				"bodyHead": "Intention (2nd)",
+				"supHTML": ""
+			},
+			{
+				"section": "55",
+				"modalHead": "Parivīmaṁsanasutta",
+				"bodyHead": "An Inquiry",
+				"supHTML": ""
+			},
+			{
+				"section": "56",
+				"modalHead": "Āneñjasappāyasutta",
+				"bodyHead": "Conducive to the Imperturbable",
+				"supHTML": ""
+			},
+			{
+				"section": "57",
+				"modalHead": "Mahānidānasutta",
+				"bodyHead": "The Great Discourse on Causation",
+				"supHTML": ""
+			},
+			{
+				"section": "62",
+				"modalHead": "Migajālattheragāthā",
+				"bodyHead": "Migajāla",
+				"supHTML": ""
+			},
+			{
+				"section": "65",
+				"modalHead": "Moḷiyaphaggunasutta",
+				"bodyHead": "Phagguna of the Top-Knot",
+				"supHTML": ""
+			},
+			{
+				"section": "66",
+				"modalHead": "Titthāyatanasutta",
+				"bodyHead": "Sectarian Tenets",
+				"supHTML": ""
+			},
+			{
+				"section": "68",
+				"modalHead": "Paṭhamamahāpañhāsutta",
+				"bodyHead": "The Great Questions (1st)",
+				"supHTML": ""
+			},
+			{
+				"section": "71",
+				"modalHead": "Paṭhamabhavasutta",
+				"bodyHead": "Continued Existence (1st)",
+				"supHTML": ""
+			},
+			{
+				"section": "76",
+				"modalHead": "Bījasutta",
+				"bodyHead": "A Seed",
+				"supHTML": ""
+			},
+			{
+				"section": "77",
+				"modalHead": "Tikaṇṇasutta",
+				"bodyHead": "With Tikaṇṇa",
+				"supHTML": "<span class='sclinktext'>AN 3.58<span class='scsegments'>:5.1–6.4</span></span>"
+			},
+			{
+				"section": "78",
+				"modalHead": "Ādittapariyāyasutta",
+				"bodyHead": "The Exposition on Burning",
+				"supHTML": ""
+			},
+			{
+				"section": "79",
+				"modalHead": "Taruṇarukkhasutta",
+				"bodyHead": "A Sapling",
+				"supHTML": ""
+			},
+			{
+				"section": "80",
+				"modalHead": "Nāmarūpasutta",
+				"bodyHead": "Name and Form",
+				"supHTML": ""
+			},
+			{
+				"section": "81",
+				"modalHead": "Viññāṇasutta",
+				"bodyHead": "Consciousness",
+				"supHTML": ""
+			},
+			{
+				"section": "83",
+				"modalHead": "Nagarasutta",
+				"bodyHead": "The City",
+				"supHTML": ""
+			},
+			{
+				"section": "84",
+				"modalHead": "Mahānidānasutta",
+				"bodyHead": "The Great Discourse on Causation",
+				"supHTML": ""
+			},
+			{
+				"section": "85",
+				"modalHead": "Subhasutta",
+				"bodyHead": "With Subha",
+				"supHTML": ""
+			},
+			{
+				"section": "87",
+				"modalHead": "Section",
+				"bodyHead": "Upaniṣads",
+				"supHTML": ""
+			},
+			{
+				"section": "88",
+				"modalHead": "Section",
+				"bodyHead": "Upaniṣads",
+				"supHTML": ""
+			},
+			{
+				"section": "89",
+				"modalHead": "Section",
+				"bodyHead": "Upaniṣads",
+				"supHTML": ""
+			},
+			{
+				"section": "90",
+				"modalHead": "Alagaddūpamasutta",
+				"bodyHead": "The Simile of the Snake",
+				"supHTML": "<span class='sclinktext'>MN 22<span class='scsegments'>:25.5–25.6</span></span> <span class='ptsref'>PTS: M I 138</span>"
+			},
+			{
+				"section": "92",
+				"modalHead": "Ajitamāṇavapucchā",
+				"bodyHead": "The Questions of Ajita",
+				"supHTML": ""
+			},
+			{
+				"section": "94",
+				"modalHead": "Section",
+				"bodyHead": "Upaniṣads",
+				"supHTML": ""
+			},
+			{
+				"section": "95",
+				"modalHead": "Naḷakalāpīsutta",
+				"bodyHead": "Bundles of Reeds",
+				"supHTML": ""
+			},
+			{
+				"section": "99",
+				"modalHead": "Mahāvedallasutta",
+				"bodyHead": "The Great Classification",
+				"supHTML": ""
+			},
+			{
+				"section": "101",
+				"modalHead": "Mahāhatthipadopamasutta",
+				"bodyHead": "The Longer Simile of the Elephant’s Footprint",
+				"supHTML": ""
+			},
+			{
+				"section": "102",
+				"modalHead": "Mahātaṇhāsaṅkhayasutta",
+				"bodyHead": "The Longer Discourse on the Ending of Craving",
+				"supHTML": ""
+			},
+			{
+				"section": "104",
+				"modalHead": "Nandakovādasutta",
+				"bodyHead": "Advice from Nandaka",
+				"supHTML": ""
+			},
+			{
+				"section": "105",
+				"modalHead": "Paṭhamadvayasutta",
+				"bodyHead": "A Duality (1st)",
+				"supHTML": ""
+			},
+			{
+				"section": "106",
+				"modalHead": "Dutiyadvayasutta",
+				"bodyHead": "A Duality (2nd)",
+				"supHTML": ""
+			},
+			{
+				"section": "108",
+				"modalHead": "Bālapaṇḍitasutta",
+				"bodyHead": "The Astute and the Foolish",
+				"supHTML": ""
+			},
+			{
+				"section": "109",
+				"modalHead": "Mahānidānasutta",
+				"bodyHead": "The Great Discourse on Causation",
+				"supHTML": ""
+			},
+			{
+				"section": "114",
+				"modalHead": "Sekhasutta",
+				"bodyHead": "A Trainee",
+				"supHTML": ""
+			},
+			{
+				"section": "115",
+				"modalHead": "Tatiyacetanāsutta",
+				"bodyHead": "Intention (3rd)",
+				"supHTML": ""
+			},
+			{
+				"section": "116",
+				"modalHead": "Section",
+				"bodyHead": "Multiple Suttas",
+				"supHTML": ""
+			},
+			{
+				"section": "118",
+				"modalHead": "Upayasutta",
+				"bodyHead": "Involvement",
+				"supHTML": ""
+			},
+			{
+				"section": "119",
+				"modalHead": "Aniccasutta",
+				"bodyHead": "Impermanence",
+				"supHTML": ""
+			},
+			{
+				"section": "120",
+				"modalHead": "Parivīmaṁsanasutta",
+				"bodyHead": "An Inquiry",
+				"supHTML": ""
+			},
+			{
+				"section": "121",
+				"modalHead": "Atthirāgasutta",
+				"bodyHead": "If There Is Desire",
+				"supHTML": ""
+			},
+			{
+				"section": "122",
+				"modalHead": "Dutiyacetanāsutta",
+				"bodyHead": "Intention (2nd)",
+				"supHTML": ""
+			},
+			{
+				"section": "126",
+				"modalHead": "Assutavāsutta",
+				"bodyHead": "Unlearned",
+				"supHTML": ""
+			},
+			{
+				"section": "134",
+				"modalHead": "Samiddhisutta",
+				"bodyHead": "With Samiddhi",
+				"supHTML": ""
+			},
+			{
+				"section": "137",
+				"modalHead": "Yamakasutta",
+				"bodyHead": "With Yamaka",
+				"supHTML": ""
+			},
+			{
+				"section": "142",
+				"modalHead": "Paṭhamanibbānapaṭisaṁyuttasutta",
+				"bodyHead": "About Extinguishment (1st)",
+				"supHTML": ""
+			},
+			{
+				"section": "146",
+				"modalHead": "Mahānidānasutta",
+				"bodyHead": "The Great Discourse on Causation",
+				"supHTML": ""
+			}
+		]
+
+
+
+		buildHTML += `<div style="margin-top: 1em; font-variant: small-caps; text-align: right"><span class="goselfquote">${linktext} in the main text</span></div>`
+
 		let selfquoteArr = document.getElementsByClassName("selfquote");
-		buildHTML = `<div style="font-variant: small-caps; text-align: right"><span class="goselfquote">${linktext} in the main text</span></div>`
 		for (i = 0; i < selfquoteArr.length; i++) {
 			if ( linktext.substring(1) == selfquoteArr[i].id.replace("bqseg", "")) {
+				let modalHeader = 'Section'
+				let supHTML = '';
+				
+				for (let j=0; j<SQHeaderArray.length; j++) {
+					if (linktext.slice(1) == SQHeaderArray[j].section) {
+						buildHTML += `<h4>${SQHeaderArray[j].bodyHead}</h4>`
+						modalHeader = SQHeaderArray[j].modalHead
+						supHTML = SQHeaderArray[j].supHTML
+					}
+				}
+
+				let bookNoteNumber = selfquoteArr[i].innerHTML.substring(selfquoteArr[i].innerHTML.lastIndexOf('<sup>')+5,selfquoteArr[i].innerHTML.lastIndexOf('</sup>'))
+				if (supHTML != '' ) {
+					buildHTML += `<h5>${supHTML}</h5>`
+				} else {
+					let allBookNotes = document.getElementsByClassName("booknotesNumber");
+					for (let k=0; k < allBookNotes.length; k++) {
+						if (allBookNotes[k].innerText == bookNoteNumber) {
+							buildHTML += `<h5>${allBookNotes[k].nextElementSibling.innerHTML}</h5>`
+						}
+					}
+				}
+
 				buildHTML += selfquoteArr[i].innerHTML.replaceAll(/ style=""/g, "").replaceAll(/<sup>[0-9]+<\/sup>/g, "").replaceAll(/<span class="goselfquote">(§[0-9]+)<\/span>/g, '$1' )
-				document.getElementById('ModalHeaderText').innerHTML = `QUOTED SECTION: ${linktext}`;
+
+				document.getElementById('ModalHeaderText').innerHTML = `${modalHeader} :: ${linktext}`;
 			}
 			
 		}
+
 	} else { // it's (currently) a Figure
-		//console.log(linktext)
 		let figureID = linktext.replace(/&nbsp;/, '').replace(' ','').toLowerCase().replace('ure', '')
 		buildHTML = `<div style="text-align: center">`
 		buildHTML += document.getElementById(figureID).innerHTML
